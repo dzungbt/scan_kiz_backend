e28249b62d3bab0b325a232cdab488c69a10626d
dungbt-dev <thedung.1292@gmail.com>
2024-12-02 14:55:16 +0700

    update sysport

--- app.js
+++ app.js
@@ -33,25 +33,25 @@ app.use('/product-images', express.static('uploads/products'));
 app.use('/request-files', express.static('uploads/requests'));
 app.use('/category-logo', express.static('uploads/categoryLogos'));
 cronJobs();
-app.use(cors({ origin: true }));
-app.use(function (req, res, next) {
+// app.use(cors({ origin: true }));
+// app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
-});
+//     // Pass to next layer of middleware
+//     next();
+// });
 //config app
 app.use(bodyParser.json({ limit: '50mb' }));
 // app.use(expressValidator());
