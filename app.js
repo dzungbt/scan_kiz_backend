e3b6e5bc46e1bd6c935aa1c0efe48cfb685f8ac8
dungbt-dev <thedung.1292@gmail.com>
2024-12-04 21:39:41 +0700

    update cors

--- app.js
+++ app.js
@@ -7,65 +7,23 @@ const cronJobs = require("./job/cronjob");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 
-
-// import express from "express";
-// import bodyParser from "body-parser";
-// import viewEngine from "./config/viewEngine";
-// import initWebRoutes from "./route/web";
-// import dotenv from "dotenv";
-// import cronJobs from './job/cronjob'
-// import cors from 'cors';
-// import nodemailer from 'nodemailer';
-
 const db = require("./models");
-// const cors = require('cors');
 
 
 dotenv.config();// required for running process.env.PORT
 let app = express();
 
-// const allowedOrigins = ['dev.kiz-scan.ru', 'kiz-scan.ru', 'www.kiz-scan.ru'];
-// app.use(cors({
-//   origin: function(origin, callback){
-//     if (!origin) {
-//       return callback(null, true);
-//     }
-
-//     if (allowedOrigins.includes(origin)) {
-//       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-//       return callback(new Error(msg), false);
-//     }
-//     return callback(null, true);
-//   }
-
-// }));
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
 
 app.use('/product-images', express.static('uploads/products'));
 app.use('/request-files', express.static('uploads/requests'));
 app.use('/category-logo', express.static('uploads/categoryLogos'));
 cronJobs();
-// app.use(cors({ origin: true }));
-// app.use(function (req, res, next) {
 
-//     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-
-//     // Pass to next layer of middleware
-//     next();
-// });
-//config app
 app.use(bodyParser.json({ limit: '50mb' }));
-// app.use(expressValidator());
 
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 initWebRoutes(app);
